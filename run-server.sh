7e41b9c53bfca9f8bd849f4dc045907c8e38196f
Minghou Ye <minghou.ye@gmail.com>
2014-03-11 11:51:02 +0800

    "docker run -name" is depreciated

--- run-server.sh
+++ run-server.sh
@@ -6,5 +6,5 @@ DL_PATH=/path/to/downloads
 TO_PATH=/path/to/torrents
 
 # Stop editing here
-docker run -p $T_PORT:9091 -v $DL_PATH:/var/lib/transmission-daemon/downloads -v $TO_PATH:/var/lib/transmission-daemon/torrents -name trans -d catatnight/transmission
+docker run -p $T_PORT:9091 -v $DL_PATH:/var/lib/transmission-daemon/downloads -v $TO_PATH:/var/lib/transmission-daemon/torrents --name trans -d catatnight/transmission
 
