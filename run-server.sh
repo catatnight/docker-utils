cadc137bf673dee9184923c4e795c257b93aa38d
Minghou Ye <minghou.ye@gmail.com>
2014-05-20 12:01:00 +0800

    add parameter "--net=host" to run-server.sh

--- run-server.sh
+++ run-server.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Configure 
-T_PORT=80
 DL_PATH=/path/to/downloads
 TO_PATH=/path/to/torrents
 
 # Stop editing here
-docker run -p $T_PORT:9091 -v $DL_PATH:/var/lib/transmission-daemon/downloads -v $TO_PATH:/var/lib/transmission-daemon/torrents --name trans -d catatnight/transmission
+docker run --net=host -v $DL_PATH:/var/lib/transmission-daemon/downloads -v $TO_PATH:/var/lib/transmission-daemon/torrents --name trans -d catatnight/transmission
 
