f5a966ab75685a36036d4f85186f22df6e204e74
Minghou Ye <minghou.ye@gmail.com>
2014-06-11 19:59:29 +0800

    define _start() and _stop()

--- manage.py
+++ manage.py
@@ -10,28 +10,26 @@ if __name__=="__main__":
   parser.add_argument("-d", "--downloads", type=str, default="", help="path to downloads directory")
   parser.add_argument("-t", "--torrents", type=str, default="", help="path to torrents-to-watch directory")
   args = parser.parse_args()
+
+  def _start(args):
+    if args.downloads == "" or args.torrents == "":
+      sys.exit("Error: paths to downloads and torrents must be set")
+    bashCommand = "docker run --net=host -v " \
+                    + args.downloads + ":/var/lib/transmission-daemon/downloads -v " \
+                    + args.torrents + ":/var/lib/transmission-daemon/torrents " \
+                    "--name transmission -d catatnight/transmission"
+    process = subprocess.Popen(shlex.split(bashCommand))
+    output = process.communicate()[0]
+
+  def _stop():
+    bashCommand = "docker rm -f transmission"
+    process = subprocess.Popen(shlex.split(bashCommand))
+    output = process.communicate()[0]
+
   if args.execute== "start":
-    if args.downloads == "" or args.torrents == "":
-      sys.exit("Error: paths to downloads and torrents must be set")
-    bashCommand = "docker run --net=host -v " \
-                    + args.downloads + ":/var/lib/transmission-daemon/downloads -v " \
-                    + args.torrents + ":/var/lib/transmission-daemon/torrents " \
-                    "--name transmission -d catatnight/transmission"
-    process = subprocess.Popen(shlex.split(bashCommand))
-    output = process.communicate()[0]
+    _start(args)
   elif args.execute== "stop":
-    bashCommand = "docker rm -f transmission"
-    process = subprocess.Popen(shlex.split(bashCommand))
-    output = process.communicate()[0]
+    _stop()
   elif args.execute== "restart":
-    bashCommand = "docker rm -f transmission"
-    process = subprocess.Popen(shlex.split(bashCommand))
-    output = process.communicate()[0]
-    if args.downloads == "" or args.torrents == "":
-      sys.exit("Error: paths to downloads and torrents must be set")
-    bashCommand = "docker run --net=host -v " \
-                    + args.downloads + ":/var/lib/transmission-daemon/downloads -v " \
-                    + args.torrents + ":/var/lib/transmission-daemon/torrents " \
-                    "--name transmission -d catatnight/transmission"
-    process = subprocess.Popen(shlex.split(bashCommand))
-    output = process.communicate()[0]
+    _stop()
+    _start(args)
