a4587d4873552cfe13cb06ecd378648d6fa8870f
Minghou Ye <minghou.ye@gmail.com>
2014-06-11 20:21:41 +0800

    error info more readable

--- manage.py
+++ manage.py
@@ -5,15 +5,16 @@ import shlex, subprocess
 import argparse
 
 if __name__=="__main__":
-  parser = argparse.ArgumentParser()
+  parser = argparse.ArgumentParser(description='Manage transmission container')
   parser.add_argument("execute", help="start|stop|restart transmission")
   parser.add_argument("-d", "--downloads", type=str, default="", help="path to downloads directory")
   parser.add_argument("-t", "--torrents", type=str, default="", help="path to torrents-to-watch directory")
   args = parser.parse_args()
 
-  def _start(args):
+  def _start(parser, args):
     if args.downloads == "" or args.torrents == "":
-      sys.exit("Error: paths to downloads and torrents must be set")
+      print parser.print_help()
+      sys.exit("Error: Paths to downloads and torrents must be set. ")
     bashCommand = "docker run --net=host -v " \
                     + args.downloads + ":/var/lib/transmission-daemon/downloads -v " \
                     + args.torrents + ":/var/lib/transmission-daemon/torrents " \
@@ -27,9 +28,10 @@ if __name__=="__main__":
     output = process.communicate()[0]
 
   if args.execute== "start":
-    _start(args)
+    _start(parser, args)
   elif args.execute== "stop":
     _stop()
   elif args.execute== "restart":
     _stop()
-    _start(args)
+    _start(parser, args)
+
